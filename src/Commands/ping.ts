28936c6be23cd02ff2939c346a530dc3d4c6ef23
LoggeL <hyper.xjo@gmx.de>
2021-08-21 23:00:37 +0200

    sample command

---
+++ src/Commands/ping.ts
@@ -0,0 +1,12 @@
+import { BaseCommand } from '@radon-types/command';
+
+module.exports = {
+    description: '',
+    name: 'ping',
+    options: {
+        adminOnly: false // demo    
+    },
+    exec: (options: object) => {
+
+    }
+}
