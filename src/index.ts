c094bf2221664898405a55129fa44291e5f52aac
LoggeL <hyper.xjo@gmx.de>
2021-08-21 23:00:17 +0200

    dynamic loading of commands

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ShardClient } from 'detritus-client';
 import { RadonClient } from './Structures/Client';
+import { readdir } from 'fs'
 
 interface RADON_ENV {
     DISCORD_TOKEN: string;
@@ -7,19 +8,35 @@ interface RADON_ENV {
 
 }
 
-
 async function main() {
     checkEnv();
     const env = process.env as unknown as RADON_ENV;
     const client = new ShardClient(env.DISCORD_TOKEN);
     const commandClient = new RadonClient(client);
     await commandClient.start();
+    loadCommands()
 }
 
-function  checkEnv() {
+function checkEnv() {
+    if (process.env.DISCORD_TOKEN) return;
+    console.error('DISCORD_TOKEN NOT PRESENT');
+    console.error('PLACE TOKEN IN .env FILE');
+    process.exit();
+}
 
-
-    
+function loadCommands() {
+    console.log('Loading Commands')
+    readdir(`${__dirname}/Commands`, (err, files: string[]) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        for (const fileName of files.filter((file) => file.endsWith('.js'))) {
+            const command = require(`${__dirname}/Commands/${fileName}`);
+            console.log('Loading Command', command.name);
+            // TODO: Command in listener
+        }
+    })
 }
 
 main().catch(console.error);
