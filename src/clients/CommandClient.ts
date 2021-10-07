2970e34731c683e4bfe3d6aea680788f9f26f47a
LoggeL <hyper.xjo@gmx.de>
2021-10-08 01:10:05 +0200

    change prefix & some renaming

--- src/clients/CommandClient.ts
+++ src/clients/CommandClient.ts
@@ -17,49 +17,58 @@ export class RadonCommandClient extends CommandClient {
         this.interactions = new InteractionClient(client);
     }
 
-
-    onPrefixCheck() { return ['x!']; }
+    onPrefixCheck() { return ['r!']; }
 
     async start() {
         try {
+
             const commandsdir = join(process.cwd(), 'dist', 'commands');
-            const commandstat  = await stat(commandsdir)
+            const commandDirExists = await stat(commandsdir)
                 .then((stat) => stat.isDirectory())
                 .catch(() => false);
+
             this.client.on('gatewayReady', () => {
+                console.log('memoryUsage')
                 console.log(process.memoryUsage());
-                
             });
-            if (commandstat) {
+
+            if (commandDirExists) {
                 await this.addMultipleIn(
-                    commandsdir, {
+                    commandsdir,
+                    {
                         isAbsolute: true,
                         subdirectories: true
                     }
                 );
             }
-            const slashCommandstat = await stat(join(process.cwd(), 'dist', 'slash-commands'))
+            const slashDirExists = await stat(join(process.cwd(), 'dist', 'slash-commands'))
                 .then((stat) => stat.isDirectory())
                 .catch(() => false);
-                console.log(slashCommandstat);
-                
-            if (slashCommandstat) {
+
+            if (slashDirExists) {
                 this.interactions.addMultipleIn(
                     join(process.cwd(), 'dist', 'slash-commands'), {
-                        isAbsolute: true,
-                        subdirectories: true
-                    });
-                    await this.interactions.checkAndUploadCommands()
-                }
+                    isAbsolute: true,
+                    subdirectories: true
+                });
+                await this.interactions.checkAndUploadCommands()
+            }
             await this.run({ wait: true });
             await this.interactions.run({ wait: true });
+
+            // const commands = await this.interactions.fetchApplicationCommands()
+            // console.log(commands.cache);
+
         } catch (error) {
             if (error instanceof Error) {
+                console.error(Object.values(Reflect.get(error, 'raw').errors)[0]);
+
+                // ToDo: Error handling breaks when description is undefined
                 console.log(
                     'error on startup',
                     Reflect.get(error, 'raw').errors.description._errors
                 );
-            } 
+            }
             else {
                 console.log('unknown error');
             }
